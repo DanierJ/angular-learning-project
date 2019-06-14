3669c7242a56ce29de1e13e6d876d50231f55df8
DanierJ <danierjavid@gmail.com>
2019-06-14 12:51:57 -0500

    Creando el Model para ingridients en la carpeta shared porque será usado por varios components

---
+++ src/app/shared/ingridient.model.ts
@@ -0,0 +1,10 @@
+export class Ingridient {
+  /*public name: string;
+  public amount: number;
+
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.amount = amount;
+  } The shortcut bellow also do the trick*/
+  constructor(public name: string, public amount: number) {}
+}
