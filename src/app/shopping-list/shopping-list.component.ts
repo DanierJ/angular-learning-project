70a9a58b5f9d667190d1950d0772c9fda76e470b
DanierJ <danierjavid@gmail.com>
2019-06-14 12:55:37 -0500

    Completando template shopping-list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingridient} from "../shared/ingridient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,17 @@ import {Component} from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
 
+  ingridients: Ingridient[] = [
+    new Ingridient('Apples', 5),
+    new Ingridient('Tomatos', 10)
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
 }
