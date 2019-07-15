2e3c3b0a499512938b4575bc3c9209400c375241
DanierJ <danierjavid@gmail.com>
2019-07-15 15:23:54 -0500

    Adding ingredients to shopping list, my way

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {ShoppingListService} from '../../shopping-list/shopping-list.service';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,4 +12,14 @@ import {Recipe} from '../recipe.model';
 
 export class RecipeDetailComponent {
   @Input() recipeDetail: Recipe;
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  addToShoppingList() {
+   this.recipeDetail.ingredients.map((
+     ingredient) => {
+     this.shoppingListService.addIngredient(new Ingredient(ingredient.name, ingredient.amount));
+   });
+  }
+
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class  RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is just a test', ''),
-    new Recipe('Another test recipe', 'This is another test', '')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel',
+      // tslint:disable-next-line:max-line-length
+      'https://www.thespruceeats.com/thmb/LeyN-7W9T0KB2nl6pcuDZckHnjc=/4288x2848/filters:fill(auto,1)/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Hamburger',
+      'This is a tasty hamburger',
+      'https://recipes-secure-graphics.grocerywebsite.com/0_GraphicsRecipes/4589_4k.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes(): Recipe[] {
