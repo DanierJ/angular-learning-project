707e018ce188d5e578760a6a065241d3a3dd04b9
DanierJ <danierjavid@gmail.com>
2019-07-15 11:39:57 -0500

    Codificando el recipe Service y utilizandolo en los components hijos

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,24 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
+  providers: [RecipeService]
 })
 
 export class RecipeListComponent implements OnInit {
-
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is just a test', ''),
-    new Recipe('Another test recipe', 'This is another test', '')
-  ];
  @Output() recipeDetail = new EventEmitter<Recipe>();
+ recipes: Recipe[];
 
-  constructor() {
+  constructor(private recipeService: RecipeService) {
 
   }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   emitDetail(recipeDetail: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,2 +1,12 @@
+import {Recipe} from './recipe.model';
+
 export class  RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is just a test', ''),
+    new Recipe('Another test recipe', 'This is another test', '')
+  ];
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice(); // Exact copy of the array
+  }
 }
