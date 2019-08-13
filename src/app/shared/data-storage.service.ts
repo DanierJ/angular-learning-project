85b4486c1ab5d74466fe11d6b55e63cb6681ee4b
DanierJ <danierjavid@gmail.com>
2019-08-13 15:26:51 -0500

    Conectando con firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
-import {Recipe} from "../recipes/recipe.model";
-import {map} from "rxjs/operators";
+import {Recipe} from '../recipes/recipe.model';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class DataStorageService {
@@ -10,11 +10,11 @@ export class DataStorageService {
   constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
   storeRecipes() {
-    return this.http.put('', this.recipeService.getRecipes());
+    return this.http.put('https://ng-recipe-book-8881f.firebaseio.com/recipes.json', this.recipeService.getRecipes());
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>('')
+    return this.http.get<Recipe[]>('https://ng-recipe-book-8881f.firebaseio.com/recipes.json')
       .pipe(
         map(recipes => {
           for (const recipe of recipes) {
