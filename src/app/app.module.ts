9e03fc468bc70da7c3da23ba7d6d776a86fc584b
DanierJ <danierjavid@gmail.com>
2019-08-20 22:24:14 -0500

    Reestructuring app with core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
-import { HeaderComponent } from './header/header.component';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 
-import {RecipeService} from './recipes/recipe.service';
 import {HttpClientModule} from '@angular/common/http';
-import {DataStorageService} from './shared/data-storage.service';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
-import {AuthService} from './auth/auth.service';
-import {AuthGuard} from './auth/auth-guard';
-import { HomeComponent } from './home/home.component';
+import {CoreModule} from './core/core.module';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -31,9 +22,8 @@ import { HomeComponent } from './home/home.component';
     AuthModule,
     SharedModule,
     ShoppingListModule,
-    AuthModule
+    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
